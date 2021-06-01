53333c64feab4969c5cdb047ec878e39eae66ad1
goodking <771691148@qq.com>
2021-06-01 10:01:55 +0800

    自定义验证码MyGoogleKaptcha提交

--- msb-dongbao-application/msb-dongbao-portal-web/src/main/java/com/msb/dongbao/common/controller/KCaptchaController.java
+++ msb-dongbao-application/msb-dongbao-portal-web/src/main/java/com/msb/dongbao/common/controller/KCaptchaController.java
@@ -3,6 +3,7 @@ package com.msb.dongbao.common.controller;
 import com.baomidou.kaptcha.Kaptcha;
 import com.baomidou.kaptcha.exception.KaptchaNotFoundException;
 import com.msb.dongbao.common.base.annotations.TokenCheck;
+import com.msb.dongbao.custom.MyGoogleKaptcha;
 import com.wf.captcha.ChineseCaptcha;
 import com.wf.captcha.SpecCaptcha;
 import com.wf.captcha.utils.CaptchaUtil;
@@ -26,6 +27,9 @@ public class KCaptchaController {
     @Autowired
     private Kaptcha kaptcha;
 
+    @Autowired
+    private MyGoogleKaptcha myGoogleKaptcha;
+
     @GetMapping("/generator")
     @TokenCheck(required = false)
     public void generatorCode(HttpServletRequest request, HttpServletResponse response) {
@@ -36,16 +40,34 @@ public class KCaptchaController {
     public String verify(String verifyCode, HttpServletRequest request) {
 
         try {
-            Boolean aBoolean = kaptcha.validate(verifyCode);
+            Boolean aBoolean = kaptcha.validate(verifyCode,10);
             if (aBoolean){
-
                 return "通过";
             }
         }catch (KaptchaNotFoundException e) {
             e.printStackTrace();
         }
-
         return "不通过";
     }
 
+    @GetMapping("/generator-my")
+    @TokenCheck(required = false)
+    public void generatorCodeMy(HttpServletRequest request, HttpServletResponse response) {
+        myGoogleKaptcha.render();
+    }
+
+    @GetMapping("/verify-my")
+    @TokenCheck(required = false)
+    public String verifyMy(String verifyCode, HttpServletRequest request) {
+
+        try {
+            Boolean aBoolean = myGoogleKaptcha.validate(verifyCode,10);
+            if (aBoolean){
+                return "通过";
+            }
+        }catch (KaptchaNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "不通过";
+    }
 }
